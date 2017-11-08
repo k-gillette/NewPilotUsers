1661f458cc4767a53b23a1e690a1117760c0f501
y010swag <33465303+y010swag@users.noreply.github.com>
2017-11-07 21:28:07 -0500

    Comments
    Added comments to code.

--- AddNewPilots.ps1
+++ AddNewPilots.ps1
@@ -7,12 +7,14 @@ $Password = "Password1"
 #Set location for CSV file
 $Users = Import-Csv -Path "C:\PowerShell\NewPilots.csv"
 
+#Loop that creates each user in the CSV
 ForEach($User in $Users){
-    $FullName = $User.First + " " + $User.Last
-    $SAM = $User.First.Substring(0,1) + $User.Last
-    $UPN = $SAM + "@domain.local"
-    $Template = Get-ADUser -Identity $User.Template -Properties MemberOf, Manager, Department, Description, Title, Company
-
+    $FullName = $User.First + " " + $User.Last #User's full name
+    $SAM = $User.First.Substring(0,1) + $User.Last #samAccount name
+    $UPN = $SAM + "@domain.local" #User profile name
+    $Template = Get-ADUser -Identity $User.Template -Properties MemberOf, Manager, Department, Description, Title, Company #Template account to use for new user
+    
+    #Create a new user with the specified parameters
     New-ADUser -Name $FullName -Instance $Template `
         -Path "OU=Pilots,OU=Domain Users,DC=domain,DC=local" `
         -GivenName $User.First -Surname $User.Last -Initials $User.Initials `
